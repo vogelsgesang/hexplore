1f5a6157936f950ce21b80a36403072bd7a8be89
Adrian Vogelsgesang <vogelsgesang@gmail.com>
2020-01-11 19:05:43 +0100

    Support for highlighting address ranges`

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,56 @@
-import React, { useState } from "react";
+import React, { KeyboardEvent, CSSProperties } from "react";
 import ReactDOM from "react-dom";
-import { DataGrid } from "./DataGrid"
+import { DataGrid, HighlightRange, Range } from "./DataGrid"
 
-class App extends React.Component<any, any> {
+interface AppState {
+    cursorPosition : number;
+    selection : Range;
+    highlighted : Array<HighlightRange>;
+}
+
+const styles : Array<CSSProperties> = [
+    {height: "1em", zIndex: -1, backgroundColor: "#fcc"},
+    {height: "1em", zIndex: -1, backgroundColor: "#cfc"},
+    {height: "1em", zIndex: -1, backgroundColor: "#ccf"},
+    {marginTop: "1.1em", borderWidth:"3px", borderBottomStyle: "solid", borderColor: "red"},
+    {marginTop: "1.1em", borderWidth:"3px", borderBottomStyle: "solid", borderColor: "green"},
+    {marginTop: "1.1em", borderWidth:"3px", borderBottomStyle: "solid", borderColor: "blue"},
+];
+
+class App extends React.Component<any, AppState> {
     state = {
         cursorPosition: 0,
         selection: {from: 0, to: 1},
+        highlighted: new Array<HighlightRange>()
+    }
+    nextStyle = 0;
+
+    onKeyPress(e : KeyboardEvent<HTMLDivElement>) {
+        if ((e.key.toLowerCase() == "m") && !e.ctrlKey && !e.altKey && !e.metaKey) {
+            if (!e.shiftKey) {
+                // Add mark
+                const key = "m" + new Date().getTime();
+                const style = styles[this.nextStyle++ % styles.length];
+                const newMark : HighlightRange = {...this.state.selection, style: style, key: key};
+                const marks = this.state.highlighted.concat([newMark]);
+                this.setState({highlighted: marks, selection: {from: this.state.cursorPosition, to: this.state.cursorPosition+1}});
+            } else if (e.shiftKey) {
+                // Remove mark
+                this.setState({highlighted: []});
+                this.nextStyle = 0;
+            }
+        }
     }
 
     render() {
+        let markList = this.state.highlighted.map((e) => <div>{e.from} - {e.to}</div>);
         return (
-            <div>
+            <div onKeyPress={(e) => this.onKeyPress(e)}>
                 <DataGrid data={this.props.data}
                     cursorPosition={this.state.cursorPosition} setCursorPosition={(x) => this.setState({cursorPosition: x})}
-                    selection={this.state.selection} setSelection={(x) => this.setState({selection: x})}/>
+                    selection={this.state.selection} setSelection={(x) => this.setState({selection: x})}
+                    highlightRanges={this.state.highlighted}/>
+                <div>{markList}</div>
                 <div>position: {this.state.cursorPosition}</div>
             </div>
         );
