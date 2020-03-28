000762a6ef264c37239ae11f44469ede3bd49fc4
Adrian Vogelsgesang <vogelsgesang@gmail.com>
2020-03-28 13:27:03 +0100

    Create JupyterLab extension from cookie-cutter

---
+++ packages/ipyhexplore/ipyhexplore/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Adrian Vogelsgesang.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "jupyter-hexplore"
+module_version = "^0.0.1"
