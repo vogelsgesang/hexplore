736b725f2d0c7d2fa2c5ec76c387090f34ec9734
vogelsgesang <vogelsgesang@users.noreply.github.com>
2020-05-02 02:12:38 +0000

    deploy: 9366adf3f1bc9d3a7ec85e907088a683c99da078

---
+++ src/AddressEditor.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+import "./AddressEditor.css";
+interface AdressEditorProperties {
+    address: number;
+    setAddress: (a: number) => void;
+}
+export declare const AddressEditor: React.ForwardRefExoticComponent<AdressEditorProperties & React.RefAttributes<unknown>>;
+export {};
+//# sourceMappingURL=AddressEditor.d.ts.map
