b16c0309979b33f089722f2d9c84170188b4d2e1
vogelsgesang <vogelsgesang@users.noreply.github.com>
2020-05-18 22:51:49 +0000

    deploy: a112047da18514c7680a4a550c2346ae56d6d367

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,5 @@
 import "hexplore-hexview/dist/hexview.css";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-contexify/dist/ReactContexify.min.css";
 //# sourceMappingURL=index.d.ts.map
