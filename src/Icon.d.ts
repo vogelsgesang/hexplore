b912bc183c327b5a2419dddac479680220ec1361
vogelsgesang <vogelsgesang@users.noreply.github.com>
2020-09-12 00:24:36 +0000

    deploy: 54937d19cd5ecc603abe78c1f1831e56cfe73271

---
+++ src/Icon.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import "./Icon.css";
+export interface IconProps {
+    icon: "arrow-up" | "arrow-down" | "trash";
+}
+export declare function Icon({ icon }: IconProps): JSX.Element;
+export declare function IconLibrary(): JSX.Element;
+//# sourceMappingURL=Icon.d.ts.map
